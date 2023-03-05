d197332f4f5b620e92847716bd31a4117194bdb9
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-03-05 23:54:44 +0900

    Update stepmotor_with_cam_and_flask.py

--- stepmotor_with_cam_and_flask.py
+++ stepmotor_with_cam_and_flask.py
@@ -101,5 +101,5 @@ def start():
     startup()
     
 if __name__ == '__main__':          # 현재 파일 실행시 개발용 웹서버 구동
-    app.run(debug=True, port=8083, host='172.30.1.54')
+    app.run(debug=True, port=8083, host='119.198.9.230')
 
